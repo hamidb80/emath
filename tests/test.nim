ec1e6bcc91e142e28ebcebab842694389722eccc
hamidb80 <hr.bolouri@gmail.com>
2022-10-10 20:49:48 +0330

    add tests

--- tests/test.nim
+++ tests/test.nim
@@ -2,19 +2,35 @@ import std/unittest
 import emath
 
 
-when isMainModule:
-  for expr in [
-    "1 + 2", 
-    "-1 * -2", 
-    "1 + 2 * 3", 
-    "1 * 2 + 3", 
-    "--1 - 2 - 3",
-    "1 ^ 8 + -3 * 7 < 1",
-    "(1) + (2 * 3) - 4",
-    # "sin(3.14) + log(16, 2)",
-  ]:
-    echo "expr: ", expr
-    let ast = parse expr
-    assert isvalid ast 
-    echo expr, " = ", eval ast
-    echo "============================="
+func `~=`(f1, f2: float): bool = 
+  0.01 > abs f1 - f2
+
+template matche(expr, answer): untyped =
+  let ast = parse expr
+  check:
+    isvalid ast
+    answer ~= eval ast
+
+
+suite "operator priority":
+  for (expr, answer) in {
+    "1 + 2": 3.0,
+    "-1 * -2": 2.0,
+    "1 + 2 * 3": 7.0,
+    "1 * 2 + 3": 5.0,
+    "--1 - 2 - 3": -4.0,
+    "2 ^ 8 + -3 * 7": 235.0,
+    "-10^2": -100.0,
+  }:
+    test expr:
+      matche expr, answer
+
+suite "pars":
+  const expr = "(1) + (2 * 3) - 4"
+  test expr:
+    matche expr, 3.0
+
+# suite "fn call":
+#   const expr = "cos(3.14) + log(16, 2)"
+#   test expr:
+#     matche expr, 3.0
